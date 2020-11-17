f5842f7808430b8e79a32d3c09cf7672e266c1cc
BajaJustin <justindabaja@gmail.com>
2020-11-17 09:33:48 -0500

    Added get/post requests for shorting url

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const e = require('express');
 const app = express();
 
 // Basic Configuration
@@ -10,13 +12,58 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// array on websites
+var sites = [];
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// New url endpoint
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  var shorturl = req.params.shorturl;
+
+  console.log(sites)
+  console.log(sites.indexOf(sites[shorturl]))
+
+  // Check if user entered a shorturl
+  if(!shorturl || sites.indexOf(sites[shorturl]) === -1){
+    res.json({
+      error: 'No short URL found for the given input'
+    });
+  }else if(isNaN(parseInt(shorturl))) {
+    res.json({
+      error: 'Wrong format'
+    });
+  }else {
+    res.redirect(sites[shorturl]);
+  }
+})
+
+// Creating new shorthand url endpoint
+app.post('/api/shorturl/new', function(req, res) {
+  var url;
+  // Validate url input
+  try{
+    url = new URL(req.body.url)
+  }catch(_){
+    res.json({
+      error: 'Invalid url'
+    })
+  }
+  // If website isn't in the array, add it
+  if(!sites.includes(url.href)){
+    sites.push(url.href);
+  }
+  
+  res.json({
+    original_url: url,
+    short_url: sites.indexOf(url.href)
+  })
+  
 });
 
 app.listen(port, function() {
